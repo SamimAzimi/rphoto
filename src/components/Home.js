fddd09ca41952f1cfe4bc241965e49c85c8320f3
samim.azimi <samim.azimi@nds.gov.af>
2021-01-07 12:05:57 +0430

    removed unsed variables

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,28 @@
 // import Slider from "react-animated-slider";
 import "react-animated-slider/build/horizontal.css";
-import Couple from "../images/gardenCouple.jpeg";
+// import Couple from "../images/gardenCouple.jpeg";
 import "./home.css";
 function Home() {
-  const slides = [
-    {
-      title: "Couple Aisle HD",
-      description: "Scared Moment Capture",
-      image: Couple,
-    },
-    {
-      title: "Best Focus",
-      description: "Focus on the special Mement",
-      image: Couple,
-    },
-    {
-      title: "Moment Capture",
-      description: "Every Wedding Moment is beautiful",
-      image: Couple,
-    },
-  ];
+  // const slides = [
+  //   {
+  //     title: "Couple Aisle HD",
+  //     description: "Scared Moment Capture",
+  //     image: Couple,
+  //   },
+  //   {
+  //     title: "Best Focus",
+  //     description: "Focus on the special Mement",
+  //     image: Couple,
+  //   },
+  //   {
+  //     title: "Moment Capture",
+  //     description: "Every Wedding Moment is beautiful",
+  //     image: Couple,
+  //   },
+  // ];
   return (
     <>
+      <h1>nothing special going on here</h1>
       {/* <main>
         <Slider autoplay="600">
           {slides.map((slide, index) => (
