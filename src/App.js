4e106f3327d2df9c85d01d73a2804596b81e5bc7
samim.azimi <samim.azimi@nds.gov.af>
2021-01-14 12:22:49 +0430

    log out button added

--- src/App.js
+++ src/App.js
@@ -6,34 +6,38 @@ import Navbar from "./components/navbar";
 import Langingpage from "./components/Landingpage";
 import CoupleloginPage from "./components/CoupleloginPage";
 import React from "react";
-import { Route, Link, useHistory } from "react-router-dom";
+import { Route, useHistory, Redirect } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import Couplegallary from "./components/CoupleGallary";
 import Adminpanel from "./components/AdminPanel";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { firebaseauth } from "./firebase.config";
 function App() {
   // const [isActive, setIsActive] = useState(true);
-  // const toggleMenu = () => {
-  //   history.pushState("newdaa", "Navbar", "/navbar");
-  // };
+
   const history = useHistory();
+  const toggleMenu = () => {
+    history.push("/navbar");
+    <Redirect to={"/navbar"} />;
+  };
   return (
     <>
       <div className="pin" id="pin"></div>
       <div className="menubar" id="menubarid">
-        <Link to="/navbar">
-          <div className="icons">
-            <FontAwesomeIcon
-              // onClick={toggleMenu}
-              className="fanavbar"
-              style={{ color: "#bf8faa" }}
-              icon={faBars}
-            />
-          </div>
-        </Link>
+        {/* <Link to="/navbar"> */}
+        <div className="icons">
+          <FontAwesomeIcon
+            onClick={toggleMenu}
+            className="fanavbar"
+            style={{ color: "#bf8faa" }}
+            icon={faBars}
+          />
+        </div>
+        {/* </Link> */}
       </div>
-
+      <div className="signoutButton">
+        <button onClick={() => firebaseauth.signOut()}>Sign Out</button>
+      </div>
       <Route exact path="/" component={Langingpage} />
       <Route exact path="/navbar" component={Navbar} />
       <Route exact path="/coupleloginpage" component={CoupleloginPage} />
--- src/components/CoupleGallary.js
+++ src/components/CoupleGallary.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { firebaseauth } from "../firebase.config";
+
 // import Slider from "react-animated-slider";
 // import "react-animated-slider/build/horizontal.css";
 // import Couple from "../images/gardenCouple.jpeg";
@@ -8,7 +8,7 @@ function CoupleGallary() {
   return (
     <>
       <h1>nothing special going on here</h1>
-      <button onClick={() => firebaseauth.signOut()}>Sign Out</button>
+      <button>Sign Out</button>
     </>
   );
 }
