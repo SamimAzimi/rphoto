4f745fefbc84ea3346e6ceacefa28eed7125b882
SA <samimelite8@gmail.com>
2021-08-10 15:11:48 +0430

    anchor fake data

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -5,19 +5,19 @@ export default function SocialMedia() {
     <div class="container">
       <ul class="social">
         <li class="facebook">
-          <a href="#" class="entypo-facebook"></a>
+          <a href="www.facebook.com/rphoto" class="entypo-facebook"></a>
         </li>
         <li class="twitter">
-          <a href="#" class="entypo-twitter"></a>
+          <a href="www.facebook.com/rphoto" class="entypo-twitter"></a>
         </li>
         <li class="dribbble">
-          <a href="#" class="entypo-dribbble"></a>
+          <a href="www.facebook.com/rphoto" class="entypo-dribbble"></a>
         </li>
         <li class="behance">
-          <a href="#" class="entypo-behance"></a>
+          <a href="www.facebook.com/rphoto" class="entypo-behance"></a>
         </li>
         <li class="linked-in">
-          <a href="#" class="entypo-linkedin"></a>
+          <a href="www.facebook.com/rphoto" class="entypo-linkedin"></a>
         </li>
       </ul>
     </div>
